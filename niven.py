2bed6268dbb143b475cb622aca13dcb40113f3b4
Fepe <fepe55@gmail.com>
2018-10-05 16:31:58 -0300

    niven

---
+++ niven.py
@@ -0,0 +1,24 @@
+# flake8: noqa
+
+"""
+A niven number is a non-negative number that is divisible by the sum of its digits.
+
+Print all the niven numbers from 0 to 100 inclusive, each on their own line.
+"""
+def forma_basica():
+    for i in range(101):
+        sum = 0
+        for digit in str(i):
+            sum += int(digit)
+        if sum and not i % sum:
+            print(i)
+
+def golf():
+ for i in range(1,101):
+  if not i%sum([int(x) for x in str(i)]):
+   print(i)
+
+
+if __name__ == '__main__':
+    forma_basica()
+    # golf()
