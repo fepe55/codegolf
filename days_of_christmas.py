adadce94aca176f0c0cbcf503ce7f96b7f87f80e
Fepe <fepe55@gmail.com>
2018-10-08 19:04:02 -0300

    Optimized 12 days of Christmas

--- days_of_christmas.py
+++ days_of_christmas.py
@@ -45,11 +45,7 @@ def forma_basica():
 def golf():
  s=['Twelve Drummers Drumming,','Eleven Pipers Piping,','Ten Lords-a-Leaping,','Nine Ladies Dancing,','Eight Maids-a-Milking,','Seven Swans-a-Swimming,','Six Geese-a-Laying,','Five Gold Rings,','Four Calling Birds,','Three French Hens,','Two Turtle Doves, and','A Partridge in a Pear Tree.']
  w=['First','Second','Third','Fourth','Fifth','Sixth','Seventh','Eighth','Ninth','Tenth','Eleventh','Twelfth']
- for i,d in enumerate(w):
-  print('On the {} day of Christmas'.format(d))
-  print('My true love sent to me')
-  print('\n'.join(s[-i-1:]))
-  print()
+ for i,d in enumerate(w):print('On the %s day of Christmas\nMy true love sent to me\n'%d+'\n'.join(s[-i-1:])+'\n')
 
 
 if __name__ == '__main__':
