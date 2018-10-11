4d94bff4fb5f58cd07a060752b4ebf0337f34c55
Fepe <fepe55@gmail.com>
2018-10-11 12:24:54 -0300

    Names

--- evil.py
+++ evil.py
@@ -2,6 +2,9 @@
 """
 https://code-golf.io/evil-numbers
 
+Evil Numbers
+==============
+
 An evil number is a non-negative number that has an even number of 1s in its binary expansion.
 
 Print all the evil numbers from 0 to 50 inclusive, each on their own line.
--- niven.py
+++ niven.py
@@ -3,6 +3,9 @@
 """
 https://code-golf.io/niven-numbers
 
+Niven Numbers
+==============
+
 A niven number is a non-negative number that is divisible by the sum of its digits.
 
 Print all the niven numbers from 0 to 100 inclusive, each on their own line.
